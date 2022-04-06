f195c2ec07d1c4dfeb0260378308d77d8fc19878
Migo <saka90030@gmail.com>
2022-04-06 17:42:48 +0900

    socket connection to Elasticsearch

---
+++ socket/elastic.py
@@ -0,0 +1,52 @@
+import socket
+import json
+import base64
+import sys
+import ssl
+#  HTTP is based on TCP, and TCP is based on IP. This means HTTP is stream-oriented(TCP) and wants an IP address to work(IP)
+#  Those requirements are fulfilled by picking
+#   -  AF_INET for socket family 
+#   - SOCK_STREAM for socket type
+
+context =ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+    # We have now a working socket object configured for HTTP transmission. This type of socket is also known as stream socket.
+    sock.settimeout(3)
+
+    # The next step is to choose a web address to connect t
+
+    sock.connect((sys.argv[1],9200))
+
+# We have now a working socket object configured for HTTP transmission. This type of socket is also known as stream socket.
+
+
+# Sending data to the server
+#
+#  An HTTP communication always starts with a request made by the client (i.e. us!) with the page we want to obtain,
+#  followed by some additional information. Such request is sent as a normal text string and looks like this:
+    path = '/'
+    host = sys.argv[1]
+    token = base64.b64encode(sys.argv[2].encode("ascii"))
+    lines = [
+        'GET %s HTTP/1.1' % path,
+        'Host: %s' % host,
+        'Authorization: Basic %s' % token.decode(),
+    ]
+
+#sock.send(b"GET / HTTP/1.1\r\nContent-Type: application/json\r\nHost:10.107.11.66\r\n\r\n")
+    try:
+        sock.send(("\r\n".join(lines) + "\r\n\r\n").encode())
+        response=sock.recv(4096).decode()
+
+        separator = response.index("{")
+        result = json.loads(response[separator:])
+
+        print(result)
+    except Exception as e:
+        print()
+        print(e.args)
+
+
+
+
